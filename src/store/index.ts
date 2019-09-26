12cc52c97afeb90634ee5959ce3519ebf7a9f9f7
jckbb <josephkbburghard@gmail.com>
2019-09-26 12:17:46 -0700

    Configure react navigation middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,9 @@
 import { FirebaseState } from "services/firebase/guide/types";
 import { GuideState } from "scenes/Main/data/guide/types";
+import { NavigationState } from "react-navigation";
 
 export interface ApplicationState {
+  nav: NavigationState,
   firebase: FirebaseState,
   guide: GuideState
 };
