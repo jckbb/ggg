7279b4ba863b313f2a11a3c5c782d67025f94c2c
jckbb <josephkbburghard@gmail.com>
2019-09-26 12:18:54 -0700

    Update root reducer with navigator reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,8 +2,10 @@ import { combineReducers }  from 'redux';
 import { ApplicationState } from '.';
 import { firebaseReducer } from '../services/firebase/guide/reducer';
 import { guideReducer } from '../scenes/Main/data/guide/reducer';
+import { navReducer } from '../navigation/reducer'
 
 export default combineReducers<ApplicationState>({
+  nav: navReducer,
   firebase: firebaseReducer,
   guide: guideReducer,
 });
