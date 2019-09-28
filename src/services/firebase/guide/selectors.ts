ce653c32233e8e0d99068c8f06bc83eebabf0683
jckbb <josephkbburghard@gmail.com>
2019-09-27 19:36:43 -0700

    Update firebase selectors to select a list of game names

--- src/services/firebase/guide/selectors.ts
+++ src/services/firebase/guide/selectors.ts
@@ -4,6 +4,8 @@ import { Guide } from './types';
 
 const selectGuideById = (state: ApplicationState, props: string): Guide => state.firebase.byData[props];
 const selectGuidesCount = (state: ApplicationState): number => state.firebase.allIds.length;
+const selectGuideIds = (state: ApplicationState) => state.firebase.allIds;
+const selectGuides = (state: ApplicationState) => state.firebase.byData;
 
 export const selectBasicGuide = createSelector([selectGuideById, selectGuidesCount], (guide, count) => 
   count > 0 
@@ -19,4 +21,8 @@ export const selectBasicGuide = createSelector([selectGuideById, selectGuidesCou
       author: '',
       steps: []
     })
+);
+
+export const selectGames = createSelector([selectGuideIds, selectGuides],
+  (ids, guides) => ids.map((id: string, index: number) => guides[id].game)
 );
