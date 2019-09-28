77a4a9fba0e0ed13ce9acde73f3edeb7fb36ee4c
jckbb <josephkbburghard@gmail.com>
2019-09-27 19:27:42 -0700

    Update navigator route with Games and Guide route

--- src/navigation/routes/index.ts
+++ src/navigation/routes/index.ts
@@ -1,8 +1,15 @@
 import { createSwitchNavigator } from 'react-navigation';
-import Main from '../../scenes/Main';
+import { createStackNavigator } from 'react-navigation-stack';
+import Games from '../../scenes/Games';
+import Guide from '../../scenes/Guide';
+
+const MainStack = createStackNavigator({
+  games: { screen: Games },
+  guide: { screen: Guide }
+});
 
 const RouteApp = createSwitchNavigator({
-  app: { screen: Main }
+  app: { screen: MainStack }
 });
 
 export { RouteApp };
