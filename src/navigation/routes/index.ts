0ad8e0db45799afb2839e7ecac7f2d8f2edc87e8
jckbb <josephkbburghard@gmail.com>
2019-09-29 15:08:27 -0700

    Update routes with navigation options and set title from passed props

--- src/navigation/routes/index.ts
+++ src/navigation/routes/index.ts
@@ -2,10 +2,22 @@ import { createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Games from '../../scenes/Games';
 import Guide from '../../scenes/Guide';
+import Guides from '../../scenes/Guides';
 
 const MainStack = createStackNavigator({
   games: { screen: Games },
-  guide: { screen: Guide }
+  guides: {
+    screen: Guides,
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.getParam('title')
+    })
+  },
+  guide: {
+    screen: Guide,
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.getParam('title')
+    })
+  },
 });
 
 const RouteApp = createSwitchNavigator({
